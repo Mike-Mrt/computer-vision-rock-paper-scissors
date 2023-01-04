af84d1023cbd1f6ff94aaf96ff2ad8fd6171fdee
Mike Maruthan <m.maruthan@hotmail.co.uk>
2023-01-04 19:26:24 +0000

    Removing comments for previous tasks

--- manual_rps.py
+++ manual_rps.py
@@ -1,28 +1,28 @@
 # Importing modules necessary
 import random
 
-# # defining function for computer selection of RPS:
-# def get_computer_choice():
-#     computer_choice = random.choice(['Rock', 'Paper', 'Scissors'])
-#     return computer_choice
+# defining function for computer selection of RPS:
+def get_computer_choice():
+    computer_choice = random.choice(['Rock', 'Paper', 'Scissors'])
+    return computer_choice
 
-# # defining function to obtain user choice between RPS:
-# def get_user_choice():
-#     user_choice =  input('Please choose between Rock, Paper and Scissors: ')
-#     return user_choice
+# defining function to obtain user choice between RPS:
+def get_user_choice():
+    user_choice =  input('Please choose between Rock, Paper and Scissors: ')
+    return user_choice
 
-# # defining a function to get winner between computer and user:
-# def get_winner(computer_choice, user_choice):
-#     if computer_choice == user_choice:
-#         print('It is a tie!')
-#     elif computer_choice == 'Rock' and user_choice == 'Scissors':
-#         print('You lost')
-#     elif computer_choice == 'Paper' and user_choice == 'Rock':
-#         print('You lost')
-#     elif computer_choice == 'Scissors' and user_choice == 'Paper':
-#         print('You lost')
-#     else:
-#         print('You won!')
+# defining a function to get winner between computer and user:
+def get_winner(computer_choice, user_choice):
+    if computer_choice == user_choice:
+        print('It is a tie!')
+    elif computer_choice == 'Rock' and user_choice == 'Scissors':
+        print('You lost')
+    elif computer_choice == 'Paper' and user_choice == 'Rock':
+        print('You lost')
+    elif computer_choice == 'Scissors' and user_choice == 'Paper':
+        print('You lost')
+    else:
+        print('You won!')
 
 # Creating function to encompass all 3 functions above into one:
 def play():
